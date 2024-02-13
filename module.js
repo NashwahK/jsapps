eb9b886ff459653005e55c9d553b6bdc847ace05
NashwahK <nashkaisar@gmail.com>
2024-02-13 14:15:18 +0500

    first commit

---
+++ module.js
@@ -0,0 +1,9 @@
+
+const names = require("./names.js")
+const sayHi = require("./utils.js")
+const trial = require("./alt-flavour.js")
+
+sayHi("susan")
+sayHi(names.john)
+sayHi(names.peter)
+console.log(trial)
